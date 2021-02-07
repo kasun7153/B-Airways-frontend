4cc0a9ec1695a0632dc20802bc87fe7e055fc29e
LahiruKumaraHewagama <lahiruhewagama98@gmail.com>
2021-02-07 17:14:02 +0530

    details table and input  form

--- src/pages/admin/AgeFilter.js
+++ src/pages/admin/AgeFilter.js
@@ -1,19 +1,92 @@
 import React, { Component } from "react";
 import Nav from "./Nav";
 import PulseLoader from "react-spinners/PulseLoader";
-import history from "../../utils/history";
 import { axiosGetInstance } from "../../axios/axios";
-import { ToastContainer, toast } from "react-toastify";
+import Table from "../table/Table";
+
 
 class AgeFilter extends Component {
   constructor(props) {
     super(props);
     this.state = {
+           errors: {},
       flightId: "",
-      passDetails: "",
+      abovePassDetails: null,
+      belowPassDetails: null,
+
       loading: false,
+      columns: [
+        {
+          Header: "User Id",
+          accessor: "user_id",
+        },
+        {
+          Header: "Name",
+          accessor: "name",
+        },
+        {
+          Header: "Email",
+          accessor: "email",
+        },
+        {
+          Header: "Birthday",
+          accessor: "birthday" ,
+        },
+       
+        {
+          Header: "Country",
+          accessor: "country",
+        },
+        {
+          Header: "Age",
+          accessor: "age",
+        },
+        {
+          Header: "Contact No",
+          accessor: "contact_no",
+        },
+        {
+          Header: "Booking Id",
+          accessor: "booking_id",
+        },
+        {
+          Header: "Seat Id",
+          accessor: "seat_id",
+        },
+        {
+          Header: "Package",
+          accessor: "package_name",
+        },
+
+        {
+          Header: "Price",
+          accessor: "price",
+        },
+      ],
     };
   }
+
+  handleValidation() {
+     let errors = {};
+    let formIsValid = true;
+
+    
+    if (!this.state.flightId) {
+      formIsValid = false;
+      errors["flightId"] = "FlightId cannot be empty!";
+    }
+
+    else{
+      if (!this.state.flightId.match(/^[0-9]+$/)) {
+        formIsValid = false;
+        errors["flightId"] = "Only numbers should be in 'FlightId' ";
+      }
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
 
   setValues = (type, e) => {
     if (type === "flightId") {
@@ -23,24 +96,49 @@ class AgeFilter extends Component {
   };
 
   getPassengersDetails = () => {
-    this.setState({ loading: true });
-    axiosGetInstance()
-      .get(`/passagedetails/${this.state.flightId}`)
-      .then((result) => {
-        this.setState({ loading: false });
-        this.setState({ passDetails: result.data.data });
-        console.log(result.data.data);
-      })
-      .catch((err) => {
-        console.log("Error getting passengers details");
-      });
+   
+    if (this.handleValidation()) {
+      
+    this.state.abovePassDetails = [];
+    this.state.belowPassDetails = [];
+  
+     
+      this.setState({ loading: true });
+      alert("Successfuly Submitted");
+      axiosGetInstance()
+        .get(`admin/passagedetails/above/${this.state.flightId}`)
+        .then((result) => {
+          this.setState({ loading: false });
+          this.setState({ abovePassDetails: result.data.data });
+        })
+        .catch((err) => {
+          console.log("Error getting passengers details");
+        });
+  
+      axiosGetInstance()
+        .get(`admin/passagedetails/below/${this.state.flightId}`)
+        .then((result) => {
+          this.setState({ loading: false });
+          this.setState({ belowPassDetails: result.data.data });
+        })
+        .catch((err) => {
+          console.log("Error getting passengers details");
+        });
+    } else {
+
+      // alert("Error getting passengers details");
+    }
+
+
+
   };
 
   render() {
     return (
       <div style={{ overflow: "hidden" }}>
+        <Nav page={"AgeFilter"}></Nav>
         <div className="relative flex flex-col h-full">
-          <Nav page={"AgeFilter"}></Nav>
+          
 
           <div
             className="flex flex-wrap content-center justify-center "
@@ -55,10 +153,13 @@ class AgeFilter extends Component {
                   placeholder="Enter Flight Id"
                   type="text"
                 ></input>
-                
-                <div  className="px-5"></div>
+               
+
+             
+                <div className="px-5"></div>
                 <div
                   onClick={this.getPassengersDetails}
+                  
                   className="w-full py-1  rounded inline-block border-2 bg-blue-900   border-blue-900 mb-5 px-1 text-white hover:shadow-2xl cursor-pointer"
                 >
                   <PulseLoader
@@ -69,12 +170,64 @@ class AgeFilter extends Component {
                   {!this.state.loading ? "SUBMIT" : null}
                 </div>
               </div>
+              <span style={{ color: "red" }}>
+                {this.state.errors["flightId"]}
+              </span>
             </div>
-
-            
           </div>
         </div>
+        {this.state.belowPassDetails == null  ? null : (
+          <>
+            <div
+              className="content-center rounded py-1 px-3 text-blue-900 "
+              style={{
+                textAlign: "center",
+                fontSize: 28,
+                fontFamily: "serif",
+                fontWeight: "bold",
+              }}
+            >
+              Details of passengers below 18 years of age
+            </div>
+            <div style={{ overflow: "auto" }}>
+              <div className="flex flex-wrap content-center rounded py-1 px-3 text-blue-900 item-center">
+                <Table
+                  data={this.state.belowPassDetails}
+                  columns={this.state.columns}
+                />
+              </div>
+            </div>{" "}
+          </>
+        )}
+        <br /> <br />
+        <br /> <br />
+        {this.state.abovePassDetails == null ? null : (
+          <>
+            <div
+              className="content-center rounded py-1 px-3 text-blue-900 "
+              style={{
+                textAlign: "center",
+                fontSize: 28,
+                fontFamily: "serif",
+                fontWeight: "bold",
+              }}
+            >
+              Details of passengers above 18 years of age
+            </div>
+            <div style={{ overflow: "auto" }}>
+              <div className="flex flex-wrap content-center rounded py-1 px-3 text-blue-900 item-center">
+                <Table
+                  data={this.state.abovePassDetails}
+                  columns={this.state.columns}
+                />
+              </div>
+            </div>{" "}
+          </>
+        )}
+        <br /> <br />
+       
       </div>
+      
     );
   }
 }
--- src/pages/admin/CountDestination.js
+++ src/pages/admin/CountDestination.js
@@ -4,14 +4,16 @@ import PulseLoader from "react-spinners/PulseLoader";
 import history from "../../utils/history";
 import { axiosGetInstance } from "../../axios/axios";
 import { ToastContainer, toast } from "react-toastify";
+import moment from "moment";
 
 class CountDestination extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      errors: {},
       formdata: {
-        startDate: "",
-        endDate: "",
+        start_date: "",
+        end_date: "",
         destination: "",
       },
       count: "",
@@ -19,30 +21,72 @@ class CountDestination extends Component {
       loading: false,
     };
   }
+
+  
+  handleValidation() {
+    let errors = {};
+   let formIsValid = true;
+
+  
+   if (!this.state.formdata.start_date) {
+     formIsValid = false;
+     errors["start_date"] = "'Start Date' cannot be empty !";
+   }
+
+   if (!this.state.formdata.end_date) {
+    formIsValid = false;
+    errors["end_date"] = "'End Date' cannot be empty !";
+    }
+    if (!this.state.formdata.destination) {
+      formIsValid = false;
+      errors["destination"] = "'Destination' cannot be empty !";
+    }
+    if (this.state.formdata.end_date<=this.state.formdata.start_date &&  formIsValid) {
+      formIsValid = false;
+      errors["date_range"] = "'Start Date' should be less than 'End Date' !";
+    }
+   this.setState({ errors: errors });
+   return formIsValid;
+ }
+
   setValues = (type, e) => {
-    if (type === "startDate") {
-      this.setState({ flightId: e.target.value });
-    } else if (type === "endDate") {
-      this.setState({ endDate: e.target.value });
+    if (type === "start_date") {
+      console.log(e.target.value);
+      this.setState({
+        formdata: { ...this.state.formdata, start_date: e.target.value },
+      });
+    } else if (type === "end_date") {
+      this.setState({
+        formdata: { ...this.state.formdata, end_date: e.target.value },
+      });
     } else if (type === "destination") {
-      this.setState({ destination: e.target.value });
+      this.setState({
+        formdata: { ...this.state.formdata, destination: e.target.value },
+      });
     }
   };
 
   getPassengersDetails = () => {
-    this.setState({ loading: true });
-    console.log(this.state.flightId);
+    if (this.handleValidation()) {
+     
+      this.setState({ loading: true });
+      alert("Successfuly Submitted");
+
     axiosGetInstance()
-      .post(`/passdescount`, this.state.formdata)
+      .post(`admin/passdescount`, this.state.formdata)
       .then((result) => {
         this.setState({ loading: false });
-        this.setState({ count: result.data.data });
+        this.setState({ count: result.data.data.pass_des_count });
         this.setState({ countState: true });
-        console.log(result.data.data);
       })
       .catch((err) => {
         console.log("Error getting count");
       });
+    } else {
+
+      // alert("Error getting passengers details");
+    }
+
   };
 
   render() {
@@ -53,35 +97,89 @@ class CountDestination extends Component {
 
           <div
             className="flex flex-wrap content-center justify-center "
-            style={{ height: "50vh" }}
+            style={{ height: "70vh" }}
           >
-            <div className="text-center w-90 border-2 p-10 rounded ">
-              <input
-                onChange={(e) => this.setValues("startDate", e)}
-                value={this.state.startDate}
-                className="w-full py-1   rounded bg-blue-50 inline-block border-2 border-blue-900 mb-5 px-2"
-                placeholder="Enter Start Date"
-                type="text"
-              ></input>
-              <input
-                onChange={(e) => this.setValues("endDate", e)}
-                value={this.state.endDate}
-                className="w-full py-1   rounded bg-blue-50 inline-block border-2 border-blue-900 mb-5 px-2"
-                placeholder="Enter End Date"
-                type="text"
-              ></input>
+            <div className=" border-2 p-10 rounded ">
+              <form className="z-10 flex sticky top-0 w-full justify-between py-1  px-2">
+                <div className="z-10 flex sticky top-0 w-full   text-blue-900  justify-between py-1  px-2">
+                  {" "}
+                  <label
+                    style={{
+                      fontSize: 15,
+                      fontWeight: "bold"
+                    }}
+                  >
+                    SELECT START  DATE{" "}
+                  </label>
+                </div>
+                <input
+                  onChange={(e) => this.setValues("start_date", e)}
+                  value={this.state.startDate}
+                  className="w-full    rounded bg-blue-50 inline-block border-2 border-blue-900 mb-5 px-2"
+                  type="date"
+                ></input>
 
-              <input
-                onChange={(e) => this.setValues("destination", e)}
-                value={this.state.destination}
-                className="w-full py-1   rounded bg-blue-50 inline-block border-2 border-blue-900 mb-5 px-2"
-                placeholder="Enter Destination"
-                type="text"
-              ></input>
 
-              <div className="flex justify-between">
-                <div></div>
+              </form>
+             < div  className="text-center"> <span style={{ color: "red" }}>
+                {this.state.errors["start_date"]}
+              </span></div><br/>
+             
 
+              <form className="z-10 flex sticky top-0 w-full justify-between py-1  px-2">
+                <div className="z-10 flex sticky top-0 w-full  text-blue-900  justify-between py-1  px-2">
+                  {" "}
+                  <label
+                    style={{
+                      fontSize: 15,
+
+                      fontWeight: "bold",
+                    }}
+                  >
+                    SELECT END  DATE{" "}
+                  </label>
+                </div>
+                <input
+                  onChange={(e) => this.setValues("end_date", e)}
+                  value={this.state.end_date}
+                  className="w-full    rounded bg-blue-50 inline-block border-2 border-blue-900 mb-5 px-2"
+                  type="date"
+                ></input>
+              </form>
+              < div  className="text-center"> <span style={{ color: "red" }}>
+                {this.state.errors["end_date"]}
+              </span></div><br/>
+             
+
+              <form className="z-10 flex sticky top-0 w-full justify-between py-1  px-2">
+                <div className="z-10 flex sticky top-0 w-full text-blue-900 justify-between py-1  px-2">
+                  {" "}
+                  <label
+                    style={{
+                      fontSize: 15,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    SELECT DESTINATION{" "}
+                  </label>
+                </div>
+                <select
+                  value={this.state.destination}
+                  onChange={(e) => this.setValues("destination", e)}
+                  className="w-full   rounded bg-blue-50 inline-block border-2 border-blue-900 mb-5 px-2"
+                >
+                   <option  value="">destination </option>
+                  <option value="bom">BOM </option>
+                  <option value="bia">BIA</option>
+                  <option value="bkk">BKK</option>
+                </select>
+              </form>
+              < div  className="text-center"> <span style={{ color: "red" }}>
+                {this.state.errors["destination"]}
+              </span></div><br/>
+             
+
+              <div className="flex justify-center">
                 <div
                   onClick={this.getPassengersDetails}
                   className="flex rounded py-1 px-8 border-2 border-blue-900 bg-blue-900 text-white hover:shadow-2xl cursor-pointer"
@@ -93,8 +191,13 @@ class CountDestination extends Component {
                   />
                   {!this.state.loading ? "SUBMIT" : null}
                 </div>
+                
               </div>
+              < div  className="text-center"> <span style={{ color: "red" }}>
+                {this.state.errors["date_range"]}
+              </span></div><br/>
             </div>
+           
           </div>
           {this.state.countState ? (
             <div
