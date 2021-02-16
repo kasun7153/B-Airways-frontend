73eb6bb06050dcde4d6b9eed56262ff83b9a9817
Lahiru kumara <62339931+LahiruKumaraHewagama@users.noreply.github.com>
2021-02-16 08:34:49 +0530

    Update AdminHome.js

--- src/pages/admin/AdminHome.js
+++ src/pages/admin/AdminHome.js
@@ -17,37 +17,22 @@ function AdminHome(props) {
   }, [props.user]);
 
   return (
-    <div style={{ overflow: "hidden" }}>
-      <div className="relative flex flex-col h-full">
-        <Nav page={"AdminHome"}></Nav>
+<div style={{ overflow: "hidden" }}>
+  <div className="relative flex flex-col h-full">
+  <Nav page={"AdminHome"}></Nav>
+  <div className="overflow-hidden">
+    <img alt="img" className=" h-screen object-cover w-full" src="/assets/background - admin.jpeg" />
+  </div>
 
-        <div className=" flex sticky top-0 w-full justify-between">
-          <div>
-            <img
-              alt="img"
-              className=" h-screen  w-25 px-1"
-              src="/assets/background - admin.jpeg"
-            />
-          </div>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 
-                  <div className="flex flex-wrap content-center justify-center">
-                      
-            <div
-              onClick={() => {
-                history.push("/admin/CreateFlight");
-              }}
-              style={{ width: "200px", height: "50px" }}
-              className=" flex-wrap flex justify-center  text-white font-bold bg-blue-900 cursor-pointer rounded border-2 border-blue-900 hover:border-white p-1 hover:shadow-2xl"
-            >
-              <div>Create New Flight</div>
-            </div>
-                  </div>
-                  
-                  <div></div>
-        </div>
-      </div>
+  <div className="absolute transform -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2 border-2 border-blue-900 px-2 py-2">
+    <div onClick={() => { history.push("/admin/CreateFlight"); }} style={{ width: "200px", height: "50px" }} className="flex-wrap flex justify-center content-center text-white font-bold bg-blue-900 cursor-pointer rounded border-2 border-blue-900 hover:border-white p-1 hover:shadow-2xl">
+      <div>Create New Flight</div>
     </div>
-  );
-}
+  </div>
 
+
+</div>
+</div>)};
 export default AdminHome;
