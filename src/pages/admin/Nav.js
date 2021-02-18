e69f3f6a016a1a0a851dd7948eed27c66f4b3be9
malithi-lalanika <66944661+malithi-lalanika@users.noreply.github.com>
2021-02-18 16:14:39 +0530

    Update Nav.js

--- src/pages/admin/Nav.js
+++ src/pages/admin/Nav.js
@@ -54,7 +54,7 @@ function Nav(props) {
             className="flex flex-wrap content-center item-center"
           >
             <div className="flex w-50 mr-2 rounded py-1 px-3 text-blue-900   hover:text-white">
-            {selectedPage("Passengers with date range of a aircraft","PassCount")}
+            {selectedPage("Number of Bookings by each passenger type with date range","PassCount")}
           </div>
         </Link>
 
